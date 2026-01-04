bd5c3d34bd260850f3e376bdbd3400146f49154e
ashishkira <ashishsingh0p768@gmail.com>
2026-01-04 13:03:52 +0530

    Quest 001 Complete: Hello World Variation.

--- 100-Python-Projects/001-hello-world.py
+++ 100-Python-Projects/001-hello-world.py
@@ -1,7 +1,11 @@
+# Normal Print method
 print("hello world")
 
+# print with a variable
 message = "Game start !"
 print(message)
 
+
+# print with f-string
 player_name = "kira"
 print(f'welcome, {player_name} to your journey')
