7caa83d1626c35852fb718cc6ad2af3a1c30b0a3
ashishkira <ashishsingh0p768@gmail.com>
2026-01-07 23:42:42 +0530

    simple calculater level 1

---
+++ 100-Python-Projects/002-simple-calculator.py
@@ -0,0 +1,14 @@
+print("--- LEVEL 1 CALCULATOR ---")
+
+num1 = input("enter first number : ")
+num2 = input("enter first number : ")
+
+n1 = float(num1)
+n2 = float(num2)
+
+print(f"Addition : {n1 + n2}")
+print(f"Subtraction : {n1 - n2}")
+print(f"Multiplication : {n1 * n2}")
+print(f"Division : {n1 / n2}")
+
+print("--- Calculation Complete ---")
